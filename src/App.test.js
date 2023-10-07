e93be139ca09c23d74d02ec4d246b8e181f688db
92FM <00092fm@gmail.com>
2023-10-07 00:26:48 -0400

    Some template cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// default test built by create-react-app, currently fails
+// we should probably make tests of our own
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
